5335d3f7945bab4ac6bb8c04028c6a461d75b0aa
jrguill3n <101654057+jrguill3n@users.noreply.github.com>
2022-03-15 22:04:20 -0500

    commit

--- bot.js
+++ bot.js
@@ -92,5 +92,8 @@ async function gotMessage(msg) {
     const index = Math.floor(Math.random() * replies.length);
     msg.channel.send({ files: [ boo[index]] });
   }
+  else if (tokens[0] === "!vern") {
+    msg.channel.send({ files: [ 'img/vern.gif'] });
+  }
 
 }
